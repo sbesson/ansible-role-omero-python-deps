87b39875b1cd3d937707be163577fc770817cf44
Sebastien Besson <seb.besson@gmail.com>
2019-02-21 19:25:20 +0000

    Migrate tests to Molecule 2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+@pytest.mark.parametrize("package", [
+    'Cython', 'numpy', 'python-jinja2', 'python-matplotlib', 'python-numexpr',
+    'python-pillow', 'python-pip', 'python-tables', 'PyYAML'])
+def test_packages(host, package):
+    assert host.package(package).is_installed
